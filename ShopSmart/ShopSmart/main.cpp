f9b44adbe0ddc5aedb7f3f329c5c54d2129985ec
MrgnHarrisn <63645728+MrgnHarrisn@users.noreply.github.com>
2023-08-25 22:13:44 +1200

    Window Position Moving Correctly

--- ShopSmart/ShopSmart/main.cpp
+++ ShopSmart/ShopSmart/main.cpp
@@ -11,26 +11,34 @@ using namespace sf;
 
 const float RESOLUTION = 16 / 9;
 const int WIDTH = 1000;
+const int HALF_WIDTH = WIDTH / 2;
 const int HEIGHT = 563;
+const int HALF_HEIGHT = HEIGHT / 2;
 
-int imgui_flags = ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove;
+int imgui_flags = ImGuiWindowFlags_NoCollapse;
+
+Vector2i sfmlWindowPos;
+ImVec2 prevPos(0, 0);
 
 int main()
 {
 
 	SupermarketAPI sapi;
 
+	ImVec2 currentWindowPos;
+
 	/* This is used to see if we actually close the window or not */
 	bool opened = true;
 
-	/* Get all supermarkets */
+	/* Get all supermarkets on startup */
+	/* Should be done on a seperate thread as to make the app look like t runs immediately */
 	auto supermarkets = sapi.fetchSupermarkets();
 
 	// SFML BAD! MUST CHANGE
 	/* Uses too many resources for this */
-	RenderWindow window(VideoMode(WIDTH, HEIGHT), "Shop Smart", Style::Close | Style::None);
-
+	RenderWindow window(VideoMode(WIDTH, HEIGHT), "Shop Smart", /*Style::Close |*/ Style::None);
 	ImGui::SFML::Init(window);
+	// ImGui::SetNextWindowPos(ImVec2(0, 0));
 	/* We need this probably for deltaTime stuff inside ImGui::SFML */
 	Clock clock;
 
@@ -47,15 +55,38 @@ int main()
 			}
 		}
 
-		/* Render Window Stuff*/
+		/* Render Window Stuff with delta time to make it smooooooth */
 		ImGui::SFML::Update(window, clock.restart());
 
+		
+
 		ImGui::Begin("ShopSmart", &opened, imgui_flags);
-			ImGui::SetWindowSize(ImVec2(WIDTH, HEIGHT));
+			/* Get the current window position */
+			currentWindowPos = ImGui::GetWindowPos();
 			ImGui::SetWindowPos(ImVec2(0, 0));
+			ImGui::SetWindowSize(ImVec2(WIDTH, HEIGHT));
 			ImGui::Button("Click Me");
 		ImGui::End();
 
+		/*sf::Vector2i sfmlWindowPosition(
+			static_cast<int>(currentWindowPos.x),
+			static_cast<int>(currentWindowPos.y + ImGui::GetStyle().FramePadding.y)
+		);*/
+
+
+		/* Get difference between ImGUI window and SFML window and add that to the SFML window position */
+		float xDiff = currentWindowPos.x - prevPos.x;
+		float yDiff = currentWindowPos.y - prevPos.y;
+
+		sfmlWindowPos.x += xDiff;
+		sfmlWindowPos.y += yDiff;
+
+		/*sfmlWindowPos = Vector2i(
+			static_cast<int>(xDiff + window.getPosition().x + HALF_WIDTH),
+			static_cast<int>(yDiff + window.getPosition().y + ImGui::GetStyle().FramePadding.y + HALF_HEIGHT)
+		);*/
+
+		window.setPosition(sfmlWindowPos);
 		window.clear();
 		ImGui::SFML::Render(window);
 		window.display();
